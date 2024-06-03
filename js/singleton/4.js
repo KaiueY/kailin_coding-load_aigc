cddbacd3e02e8ca23cdefec828294f1fcd47fb39
KaieuY <2899492106@qq.com>
2024-06-03 12:26:44 +0800

    nothing

--- js/singleton/4.js
+++ js/singleton/4.js
@@ -1,6 +1,4 @@
-function Duck() {
 
-}
 //函数表达式
 //js 变量类型由值决定
 var Singleton = function (name) {
@@ -8,9 +6,5 @@ var Singleton = function (name) {
 }
 var obj1 = new Singleton('kkk')
 var obj2 = new Singleton('kkk')
-console.log(obj3 === obj2, obj1.name === obj2.name);
-
-var obj={'a':1}
-var obj3={'a':1}
-var obj4=obj 
-console.log(obj.a ,obj4==obj3); 
+var obj3=obj2
+console.log(obj3 === obj2);
--- js/singleton/6.js
+++ js/singleton/6.js
@@ -4,53 +4,49 @@
  * @param  {...any} args - 传递给构造函数的参数
  * @returns {Object} - 返回构造函数的实例
  */
-function instantiate(Constructor, ...args) {
-    // 1. 检查第一个参数是否是函数
-    if (typeof Constructor !== 'function') {
-      throw new Error('第一个参数必须是构造函数');
-    }
-  
-    // 2. 创建一个新的空对象
-    const instance = {};
-  
-    // 3. 将新对象的原型设置为构造函数的原型
-    // 这样新对象可以访问构造函数原型链上的属性和方法
-    instance.__proto__ = Object.create(Constructor.prototype);
-  
-    // 4. 调用构造函数，并将 this 绑定到新创建的对象上
-    const result = Constructor.apply(instance, args);
-  
-    // 5. 如果构造函数返回的是对象或函数，则返回该结果
-    // 否则返回新创建的对象
-    const isObject = typeof result === 'object' && result !== null;
-    const isFunction = typeof result === 'function';
-    return isObject || isFunction ? result : instance;
+function ourNew(Constructor, ...args) {
+  //  检查第一个参数是否是函数
+  if (typeof Constructor !== 'function') {
+    throw new Error('第一个参数必须是构造函数');
   }
-  
-  // 测试用的构造函数
-  function Person(name) {
-    this.name = name;
-  
-    // 返回一个函数用于测试
-    return function() { // 用来测试第 5 点
-      console.log('返回引用数据类型');
-    };
-  }
-  
-  // 在构造函数的原型上添加一个方法
-  Person.prototype.sayName = function() {
-    console.log(`My name is ${this.name}`);
-  }
-  
-  // 创建一个 Person 实例
-  const me = instantiate(Person, 'Alice'); // 用来测试第 4 点
-  
-  // 调用实例方法
-  me.sayName(); // My name is Alice
-  
-  // 输出实例对象
-  console.log(me); // Person {name: 'Alice'}
-  
-  // 用来测试第 1 点
-  // const invalidInstance = instantiate({ name: 'Alice' }, 'Alice'); // 报错：第一个参数必须是构造函数
-  
+
+  // 创建一个新的空对象
+  const obj = {};
+
+  // 将新对象的原型设置为构造函数的原型
+  // 这样新对象可以访问构造函数原型链上的属性和方法
+  obj.__proto__ = Object.create(Constructor.prototype);
+
+  // 调用构造函数，并将 this 绑定到新创建的对象上
+  const result = Constructor.apply(obj, args);
+
+  //  如果构造函数返回的是对象或函数，则返回该结果
+  // 否则返回新创建的对象
+  const isObject = typeof result === 'object' && result !== null;
+  const isFunction = typeof result === 'function';
+  return isObject || isFunction ? result : obj;
+}
+
+// 测试用的构造函数
+function Constructor(name) {
+  this.name = name;
+
+  // 返回一个函数用于测试
+  return function() { 
+    console.log('返回引用数据类型');
+  };
+}
+
+// 在构造函数的原型上添加一个方法
+Constructor.prototype.sayName = function() {
+  console.log(`My name is ${this.name}`);
+}
+
+// 创建一个 Constructor 实例
+const me = ourNew(Constructor, 'kailin'); 
+
+// 调用实例方法
+me.sayName(); 
+
+// 输出实例对象
+console.log(me); 
