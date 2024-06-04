4db02eeee91e6a3bd305fc07d08f29f90887b1a7
KaieuY <2899492106@qq.com>
2024-06-04 11:45:19 +0800

    重复声明

---
+++ random/nothing.js
@@ -0,0 +1,27 @@
+    // var d = 3;
+    // console.log(typeof (d));
+    // function d() {
+    // }
+    // console.log(typeof (d));
+
+// function d() {}
+// console.log(typeof (d));//function
+// var d = 3;
+// console.log(typeof (d));//number
+
+// var d 
+// console.log(d)//[Function: d]
+// function d(){}
+// console.log(d)//[Function: d]
+// console.log(d)//[Function: d]
+
+// function d(){}
+// d=2
+// console.log(d);//2
+
+
+var d=3
+d=function name(params) {
+    
+}
+console.log(d);//[Function: name]
--- random/youdn1.js
+++ random/youdn1.js
@@ -2,10 +2,11 @@ function foo() {
     function bar(a) {
         i=3
         console.log(a+i);
-        
     }
     for(let i=0; i<10; i++) {
         bar(i*2)
     }  
 }
 foo();
+console.log(i);
+// note 在这段代码中，由于var 声明的i 在整个
