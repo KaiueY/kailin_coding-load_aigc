8ce402786abb8831b3ec899b24c5ac6e5bb142f5
KaieuY <2899492106@qq.com>
2024-09-19 21:32:57 +0800

    element

--- Vue/alimente/src/App.vue
+++ Vue/alimente/src/App.vue
@@ -1,18 +1,26 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <div>
-    <ElDilog/>
-    <el-dialog></el-dialog>
+    
+    <el-dialog>
+      <iframe
+        src="//player.bilibili.com/player.html?aid=673762230&bvid=BV1pU4y1G7VX&cid=358701287&page=1"
+        scrolling="no"
+        border="0"
+        frameborder="no"
+        framespacing="0"
+        allowfullscreen="true"
+      />
+    </el-dialog>
   </div>
 </template>
 
 <style scoped>
 .logo {
-  height: 6em;
-  padding: 1.5em;
+  height: 6rem;
+  padding: 1.5rem;
   will-change: filter;
   transition: filter 300ms;
 }
--- Vue/alimente/src/components/dialog/Dialog.vue
+++ Vue/alimente/src/components/dialog/Dialog.vue
@@ -1,17 +1,59 @@
 <template>
-    <div>
-    dialog
-    </div>
+    <teleport to="body">
+        <div class="el-dialog">
+            <div class="el-dialog__content">
+                <slot   />
+            </div>
+        </div>
+    </teleport>
 </template>
 
-<script  lang="ts">
-export default {
-    name:'ElDialog'
-
+<script lang="ts">
+import { withDefaults } from 'vue';
+interface Props {
+    appendToBody:boolean
 }
+withDefaults(defineProps<Props>(),{
+    appendToBody:true
+    
+})
+export default {
+    name :'Dialog'
+}
+
 
 </script>
 
-<style lang="css" scoped>
-    
+<style lang="css">
+.el-dialog {
+    position: fixed;
+    width: 100%;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-color: rgba(0, 0, 0, .5);
+
+}
+
+.el-dialog__content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-color: rgb(69, 197, 165);
+    width: 800px;
+    height: 400px;
+    padding: 5px;
+}
+
+iframe {
+    width: 600px;
+    height: 300px;
+}
 </style>
