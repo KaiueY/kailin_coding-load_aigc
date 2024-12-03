878eff307086fdc6b855d5ebf9335c1567573bdb
KaieuY <2899492106@qq.com>
2024-12-03 23:58:51 +0800

    解决

--- 一些算法题/leetcode/跳跃游戏.js
+++ 一些算法题/leetcode/跳跃游戏.js
@@ -33,10 +33,14 @@
  */
 var canJump = function(nums) {
     let n = nums.length
-    let max = 0
+    let dp = new Array(n).fill(false)
+    dp[0] = true
     for (let i = 0; i < n; i++) {
-        if (i > max) return false
-        max = Math.max(max, i + nums[i])
+        if (dp[i]) {
+            for (let j = i + 1; j <= i + nums[i] && j < n; j++) {
+                dp[j] = true
+            }
+        }
     }
-    return true
+    return dp[n-1]
 };
