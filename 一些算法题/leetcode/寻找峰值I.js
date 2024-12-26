6685a9e10ecd38badae3f930f15f993e78380d9e
KaiueY <2899492106@qq.com>
2024-12-26 21:26:37 +0800

    寻找峰值的几个解法

--- 一些算法题/leetcode/寻找峰值I.js
+++ 一些算法题/leetcode/寻找峰值I.js
@@ -26,5 +26,14 @@
  * @return {number}
  */
 var peakIndexInMountainArray = function(arr) {
-    
+    let left = 0, right = nums.length - 1;
+    while (left < right) {
+        const mid = Math.floor((left + right) / 2);
+        if (nums[mid] > nums[mid + 1]) {
+            right = mid;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return left;
 };
