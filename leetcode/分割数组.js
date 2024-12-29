dcc5f5f398efb7d6d6521e86bcedd3c4ed5357a9
KaieuY <2899492106@qq.com>
2024-12-29 23:50:43 +0800

    fix：forEach无法跳出改为 for……of

--- leetcode/分割数组.js
+++ leetcode/分割数组.js
@@ -4,9 +4,12 @@
  */
 var isPossibleToSplit = function(nums) {
     const map = new Map()
-    nums.forEach((element)=>{
+    for(let element of nums){
         map.set(element, (map.get(element) || 0) + 1);
         if(map.get(element) === 3) return false
-    })
+        console.log(map);
+        
+    }
     return true
 };
+console.log( isPossibleToSplit([1,1,1,1]));
