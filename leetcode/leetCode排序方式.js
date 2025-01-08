932797961dc6401503f9c1c9128e36091cbf2dae
Raphael Kai <169074373+KaiueY@users.noreply.github.com>
2025-01-08 20:30:47 +0800

    简单题目

---
+++ leetcode/leetCode排序方式.js
@@ -0,0 +1,10 @@
+// https://leetcode.cn/problems/sort-by/
+/**
+ * @param {Array} arr
+ * @param {Function} fn
+ * @return {Array}
+ */
+var sortBy = function(arr, fn) {
+    arr.sort((a,b) => fn(a)-fn(b))
+    return arr
+};
