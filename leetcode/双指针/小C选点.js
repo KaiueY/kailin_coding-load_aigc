002617f20f54322a5ed4d0bc27197af610628079
Raphael Kai <584520kailin@gmail.com>
2025-01-27 19:52:17 +0800

    nothing

---
+++ leetcode/双指针/小C选点.js
@@ -0,0 +1,14 @@
+// https://www.marscode.cn/practice/lnnw7dyrov7624?problem_id=7424418560666648620
+function solution(n, m, k, x) {
+    // PLEASE DO NOT MODIFY THE FUNCTION SIGNATURE
+    // write code here
+    return 0; // placeholder return
+}
+
+function main() {
+    console.log(solution(4, 2, 3, [1, 2, 3, 4]) === 6);
+    console.log(solution(4, 2, 2, [1, 2, 3, 4]) === 5);
+    console.log(solution(5, 3, 5, [1, 3, 6, 7, 9]) === 3);
+}
+
+main();
