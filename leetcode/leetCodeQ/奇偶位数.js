17afaa53f0fc8dbaf8cbf96657c8b62197e0a50e
Raphael Kai <584520kailin@gmail.com>
2025-02-21 23:16:39 +0800

    leetCode

---
+++ leetcode/leetCodeQ/奇偶位数.js
@@ -0,0 +1,15 @@
+// https://leetcode.cn/problems/number-of-even-and-odd-bits/?envType=daily-question&envId=2025-02-20
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var evenOddBit = function(n) {
+    const res = [0, 0];
+ let i = 0;
+ while (n > 0) {
+     res[i] += n & 1;
+     n >>= 1;
+     i ^= 1;
+ }
+ return res;
+};
