f4127ddf7e84daaca059e07372b2017a4b5a454f
Raphael Kai <584520kailin@gmail.com>
2025-03-03 23:52:50 +0800

    leetCode

---
+++ leetcode/字符串/重复的子字符串.js
@@ -0,0 +1,32 @@
+// https://leetcode.cn/problems/repeated-substring-pattern/
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var repeatedSubstringPattern = function (s) {
+    if (s.length === 0)
+        return false;
+
+    const getNext = (s) => {
+        let next = [];
+        let j = -1;
+
+        next.push(j);
+
+        for (let i = 1; i < s.length; ++i) {
+            while (j >= 0 && s[i] !== s[j + 1])
+                j = next[j];
+            if (s[i] === s[j + 1])
+                j++;
+            next.push(j);
+        }
+
+        return next;
+    }
+
+    let next = getNext(s);
+
+    if (next[next.length - 1] !== -1 && s.length % (s.length - (next[next.length - 1] + 1)) === 0)
+        return true;
+    return false;
+};
