60bf5bed82d4f963d3d513e1f87bb72084373e24
Raphael Kai <584520kailin@gmail.com>
2025-03-30 15:27:43 +0800

    feat(DP): 添加最长回文子串的中心扩散解法
    在最长回文子串问题中，新增了中心扩散算法的实现，以优化解法并提高代码的可读性

--- leetcode/DP/最大回文子串.js
+++ leetcode/DP/最大回文子串.js
@@ -1,3 +1,4 @@
+// https://leetcode.cn/problems/longest-palindromic-substring/
 /**
  * @param {string} s
  * @return {string}
@@ -39,4 +40,38 @@ var longestPalindrome = function(s) {
     }
     
     return s.substring(start, start + maxLength);  
+};
+
+/**
+ * @param {string} s
+ * @return {string}
+ * 中心扩散
+ */
+var longestPalindrome = function(s) {
+    if (s.length < 1) return "";
+    
+    let start = 0, end = 0; // 记录回文的起始和结束索引
+    
+    const expandAroundCenter = (str, left, right) => {
+        while (left >= 0 && right < str.length && str[left] === str[right]) {
+            left--;
+            right++;
+        }
+        return right - left - 1; // 返回回文长度
+    };
+    
+    for (let i = 0; i < s.length; i++) {
+        // 必须同时检查奇数和偶数情况，不能有条件判断
+        const len1 = expandAroundCenter(s, i, i);    // 奇数长度
+        const len2 = expandAroundCenter(s, i, i + 1); // 偶数长度
+        const len = Math.max(len1, len2);
+        
+        // 只有当找到更长的回文时才更新位置
+        if (len > end - start) {
+            start = i - Math.floor((len - 1) / 2);
+            end = i + Math.floor(len / 2);
+        }
+    }
+    
+    return s.substring(start, end + 1);
 };
