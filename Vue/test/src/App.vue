55cc3bd0757a07df589429ca84af4849ddd54493
Raphael Kai <584520kailin@gmail.com>
2025-05-02 23:29:04 +0800

    feat:inspiral UI

---
+++ Vue/test/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import FlipCard from './components/FlipCard.vue';
+</script>
+
+<template>
+  <div class=""><flip-card> </flip-card></div>
+  
+</template>
+
+<style scoped>
+</style>
